4400a17b5cc82d87647a87f2a844cb3b46aa1e08
rakshashetty66 <raksha.21ai033@sode-edu.in>
2024-11-23 23:43:29 +0530

    Portfolio

---
+++ script.js
@@ -0,0 +1,30 @@
+const tabs = document.querySelectorAll('.tab');
+const tabContents = document.querySelectorAll('.tab-content');
+const resumeLists=document.querySelectorAll('.resume-list');
+const resumeBoxs=document.querySelectorAll('.resume-box');
+
+
+
+resumeLists.forEach((list,idx) => {
+    list.addEventListener('click',() =>{
+        document.querySelector('.resume-list.active').classList.remove('active');
+        list.classList.add('active');
+
+        document.querySelector('.resume-box.active').classList.remove('active');
+        resumeBoxs[idx].classList.add('active');
+    });
+});
+
+
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        // Remove active class from all tabs and contents
+        tabs.forEach(t => t.classList.remove('active'));
+        tabContents.forEach(content => content.classList.remove('active'));
+
+        // Add active class to the clicked tab and target content
+        tab.classList.add('active');
+        document.querySelector(`.${tab.dataset.target}`).classList.add('active');
+    });
+});
